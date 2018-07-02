c9f6cf6887afb6d28a21575388110a2f470cd020
Kathryn Stapleton <kathryn.stapleton777@gmail.com>
2018-07-02 16:30:39 -0400

    make advert and clickbait modules

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class Header extends Component {
   render() {
-  return (
+    return (
  
   <div className="expanded row header">
     <div className="clearfix">
@@ -21,7 +21,7 @@ class Header extends Component {
     </div>
   </div>
 
-  );
+    );
   }
 }
 
